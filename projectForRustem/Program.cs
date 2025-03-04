5a162247d93e085107de2b44b3b50796a0a1f7da
Timur <timur18011@mail.ru>
2025-03-04 18:19:30 +0600

    create output method

--- projectForRustem/Program.cs
+++ projectForRustem/Program.cs
@@ -10,4 +10,28 @@
                 count++;
             }
         }
+// Создаем новый массив нужного размера
+        string[] resultArray = new string[count];
 
+        // Заполняем новый массив
+        int index = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index] = str;
+                index++;
+            }
+        }
+
+        return resultArray;
+    }    
+ static void PrintArray(string[] array)
+    {
+        foreach (string str in array)
+        {
+            Console.Write(str + " ");
+        }
+        Console.WriteLine();
+    }
+}
